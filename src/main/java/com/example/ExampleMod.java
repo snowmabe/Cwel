f8a51ce30040e1a02f4b1149f9999e336c1bf9b2
snowmabe <zagorowskigracjan@gmail.com>
2026-08-13 01:41:18 +0200

    Update ExampleMod MOD_ID to v9sndlc and change log message

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -8,7 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+	public static final String MOD_ID = "v9sndlc";
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
@@ -21,7 +21,7 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("V9SNDLC initialized");
 	}
 
 	public static Identifier id(String path) {
